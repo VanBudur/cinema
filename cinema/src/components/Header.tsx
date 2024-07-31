9318a85803f163d5a5e21301105f825edfea7de9
VanBudur <111625941+VanBudur@users.noreply.github.com>
2024-07-31 12:26:45 +0400

    g

--- cinema/src/components/Header.tsx
+++ cinema/src/components/Header.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-const Header: React.FC = () => {
-  return (
-    <header>
-        <nav className="navNo1">
+// const Header: React.FC = () => {
+//   return (
+//     <header>
+//         <nav className="navNo1">
 
-        <ul>
-          <li><a href="#home">الصفحة الرئيسية</a></li>
-          <li><a href="#about">عن المنطقة</a></li>
-          <li><a href="#contact">تواصل معنا</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+//         <ul>
+//           <li><a href="#home">الصفحة الرئيسية</a></li>
+//           <li><a href="#about">عن المنطقة</a></li>
+//           <li><a href="#contact">تواصل معنا</a></li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// };
 
-export default Header;
+// export default Header;
